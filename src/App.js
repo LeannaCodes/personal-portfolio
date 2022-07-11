c27f058ec51b2ac7f9402e4535e693e6b9da7543
PriyathamVarma <priyatham002@gmail.com>
2022-07-11 10:52:00 +0100

    some commits

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+// IMPORTS
+
 import { Animator, batch, Fade, Move, ScrollContainer, ScrollPage, Sticky, StickyIn, Zoom, ZoomIn, MoveOut } from 'react-scroll-motion';
 import './App.css';
 import AboutMe from './Components/AboutMe/AboutMe';
 import Education from './Components/Education/Education';
 import Header from './Components/Header/Header';
 import WorkExp from './Components/WorkExp/WorkExp';
+import { useState } from 'react';
+
+import Timeline from './Timeline';
 
  
 /*const ZoomInScrollOut = batch(StickyIn(), ZoomIn(), FadeIn());*/
 const FadeUp = batch(Fade(), Sticky(), Move())
 
-function App() {
+// APP function
+
+const App = () => {
+
+  // Hooks
+  const [name,setName] = useState('Leanna ;)');
+
+  // Methods
+
+  const contentChanged = (event) => {
+
+    setName(event.target.value);
+
+  }
   return (
     <ScrollContainer>
       <ScrollPage page={0}>
@@ -29,22 +47,48 @@ function App() {
      </ScrollPage>
 
      <ScrollPage page={2}>
-      <div className='education'>
+      <div className='none backgroundColorUI'>
         <Animator animation={FadeUp}>
-          <Education />
+          
+          
+        </Animator>
+      </div>
+     </ScrollPage>
+
+     <ScrollPage page={3}>
+      <div className='education backgroundColorUI'>
+        <Animator animation={FadeUp}>
+          
+          <Timeline />
+        </Animator>
+      </div>
+     </ScrollPage>
+
+     <ScrollPage page={4}>
+      <div className='none backgroundColorUI'>
+        <Animator animation={FadeUp}>
+          
+          
         </Animator>
       </div>
      </ScrollPage>
 
 
-     <ScrollPage page={3}>
+     <ScrollPage page={5}>
       <div className='work-exp'>
         <Animator animation={FadeUp}>
           <WorkExp />
+          <input type="text" placeholder="Enter your requirements" onKeyUp={(e) => contentChanged(e)}/><br/>
+          <h1>You entered:</h1>
+          {name}
         </Animator>
         </div>
      </ScrollPage>
+
+
+
     </ScrollContainer> 
+    
   );
 }
 
